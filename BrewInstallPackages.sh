3ff2fcd68fe325fb8b16bded658bd031d794dda5
rush <eightseventhreethree>
2019-03-23 02:47:46 -0400

    fixed some logic that was broken around installing on fresh ubuntu instance

--- BrewInstallPackages.sh
+++ BrewInstallPackages.sh
@@ -12,31 +12,34 @@ function install_brew_osx() {
 
 function install_brew_linux() {
   echo "Install Linux Brew"
-  echo 'export PATH="$HOME/.linuxbrew/bin:$PATH"' >>~/.bashrc
-  echo 'export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"' >>~/.bashrc
-  echo 'export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"' >>~/.bashrc
+  if grep -q "linuxbrew" ~/.bashrc; then
+    echo 'export PATH="$HOME/.linuxbrew/bin:$PATH"' >>~/.bashrc
+    echo 'export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"' >>~/.bashrc
+    echo 'export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"' >>~/.bashrc
+  fi
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
   source ~/.bashrc
   echo "Finished function: install_brew_linux"
 }
 
-function install_ruby_apt() {
-  echo "Appears that Ruby is not installed: "
-  sudo apt-get install ruby
-  sudo apt install linuxbrew-wrapper
-  echo "Finished function: install_ruby_apt"
+function install_apt() {
+  echo "Installing required packages: "
+  sudo apt-get install ruby -y
+  sudo apt-get install build-essential -y
+  sudo apt install linuxbrew-wrapper -y
+  echo "Finished function: install_apt"
 }
 
-function install_ruby_yum() {
-  echo "Appears that Ruby is not installed: "
-  sudo yum install ruby
-  sudo yum groupinstall 'Development Tools'
-  echo "Finished function: install_ruby_yum"
+function install_yum() {
+  echo "Installing required packages: "
+  sudo yum install ruby -y
+  sudo yum groupinstall 'Development Tools' -y
+  echo "Finished function: install_yum"
 }
 
 function get_package_list() {
   echo "Getting package list from site."
-  DOWNLOAD_LIST_PACKAGE=$(wget --quiet -O - thisisrush.wordpress.com/brew-packages/ | grep '<br />' | awk -F '<' '{print $1","}')
+  DOWNLOAD_LIST_PACKAGE=$(curl -s -XGET https://thisisrush.wordpress.com/brew-packages/ | grep -vi 'site uses cookies' | grep '<br />' | awk -F '<' '{print $1","}')
   #echo "$DOWNLOAD_LIST_PACKAGE"
   declare -a LIST_PACKAGE
   IFS=','
@@ -44,73 +47,78 @@ function get_package_list() {
   echo "The number of packages found were: ${#LIST_PACKAGE[@]}"
 }
 
-ruby --version
-if [[ $? != 0 ]]; then
-  if [[ "$OS_var" == "Linux" ]]; then
-    if [[ "$distro_var" =~ ..Ubuntu ]]; then
-      install_ruby_apt
-    elif [[ "$distro_var" =~ CentOS.. ]]; then
-      install_ruby_yum
+function check_or_install_ruby() {
+  if ! ruby --version ; then
+    if [[ "$OS_var" == "Linux" ]]; then
+      if [[ "$distro_var" =~ ..Ubuntu ]]; then
+        install_apt
+      elif [[ "$distro_var" =~ CentOS.. ]]; then
+        install_yum
+      else
+        echo "Sorry you will need to install Ruby and other required packages manually\nEx. sudo dnf install ruby"
+        exit
+      fi
     else
-      echo "Sorry you will need to install Ruby manually"
-      echo "Ex. sudo dnf install ruby"
-      exit
+      echo "MacOS comes with Ruby"
+    fi
+  fi
+}
+
+function check_or_install_brew() {
+  if ! which brew >/dev/null; then
+    if [[ "$OS_var" == "Linux" ]]; then
+      install_brew_linux
+    elif [[ "$OS_var" == "Darwin" ]]; then
+      install_brew_osx
     fi
   else
-    echo "MacOS comes with Ruby"
+    brew update
   fi
-fi
-
-which brew
-if [[ $? != 0 ]]; then
-  if [[ "$OS_var" == "Linux" ]]; then
-    install_brew_linux
-  elif [[ "$OS_var" == "Darwin" ]]; then
-    install_brew_osx
+}
+function install_packages() {
+  get_package_list
+  if [ -z "$LIST_PACKAGE" ]; then
+    declare -a LIST_PACKAGE=(
+    'coreutils'
+    'binutils'
+    'diffutils'
+    'watch'
+    'wget'
+    'bash'
+    'make'
+    'nano'
+    'python'
+    'python3'
+    'rsync'
+    'gnutls'
+    'grep'
+    'gnu-which'
+    'gnu-indent'
+    'gnu-sed'
+    'gnu-tar'
+    'nmap'
+    'tcptraceroute'
+    'ack'
+    'odt2txt'
+    'parallel'
+    'rsync'
+    'perl'
+    'googler'
+    'gawk'
+    'git'
+    'carthage'
+    'maven'
+    )
+    IFS=''
   fi
-else
-  brew update
-fi
-get_package_list
-if [ -z "$LIST_PACKAGE" ]; then
-  declare -a LIST_PACKAGE=(
-  'coreutils'
-  'binutils'
-  'diffutils'
-  'watch'
-  'wget'
-  'bash'
-  'make'
-  'nano'
-  'python'
-  'python3'
-  'rsync'
-  'gnutls'
-  'grep'
-  'gnu-which'
-  'gnu-indent'
-  'gnu-sed'
-  'gnu-tar'
-  'nmap'
-  'tcptraceroute'
-  'ack'
-  'odt2txt'
-  'parallel'
-  'rsync'
-  'perl'
-  'googler'
-  'gawk'
-  'git'
-  'carthage'
-  'maven'
-  )
-  IFS=''
-fi
-for PACKAGE in "${LIST_PACKAGE[@]}"
-do
-  OUTPUT=$(brew install "$PACKAGE" 2>&1)
-  echo "$OUTPUT" | grep -i :
-done
-#V.1 - Includes brew update and check for brew.
-#V.3 - Removed errors output.
-#Rush Simonson
+  for PACKAGE in "${LIST_PACKAGE[@]}"; do
+    OUTPUT=$(brew install "$PACKAGE" 2>&1)
+    echo "$OUTPUT" | grep -i :
+  done
+}
+function main() {
+  check_or_install_ruby
+  check_or_install_brew
+  install_packages
+}
+main
